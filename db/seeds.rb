c48d7474994a88cfedb6d150cd49a9bbeafc1f44
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-14 02:02:30 +0000

    update gemfile and install

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ Customer.create!(
+      email: "abc@gmail.com",
+      first_name: "太郎",
+      last_name: "山田",
+      first_name_kana: "タロウ",
+      last_name_kana: "ヤマダ",
+      password: "abcabc",
+      postcode: "1234567",
+      address: "東京都渋谷区神南1丁目19-11 パークウェースクエア2",
+      phone_number: "09012345678"
+    )
