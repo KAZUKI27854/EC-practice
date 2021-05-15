43887c4ffeac5ccfe106f4444fce0da457a1bfd5
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-15 12:07:47 +0000

    tmp saving for order

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -1,9 +1,31 @@
 class Customer::OrdersController < ApplicationController
   def new
     @order = Order.new
-    @address = Address.where(customer_id: current_customer)
+    @current_customer = Customer.first
+    @customer_postcode = @current_customer.postcode
+    @customer_address = @current_customer.address
+    #@customer_address = current_customer.address
+    #@delivery_address_list = Delivery.where(customer_id: current_customer.id)
+
+    #cart_items = current_customer.cart_items
+    #if cart_items.empty?
+       #redirect_to customers_cart_items_path
+    #end
   end
-  
+
+  def confirm
+    @order = Order.new(order_params)
+  end
+
   def create
+    @order = current_customer.order.new(order_params)
+  end
+
+  private
+
+  def order_params
+   params.require(:order).permit(:customer_id, :postage, :total_price, :payment_method, :status,
+                                 :delivery_name, :delivery_postcode, :delivery_address,
+   order_items_attributes:[:item_id, :order_id, :quantity, :market_price, :product_status])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   namespace :customer do
     resources :orders
+    post '/confirm' => 'orders#confirm'
   end
 end
