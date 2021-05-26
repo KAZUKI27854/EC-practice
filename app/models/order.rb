80d72b13bc9e9d7b5a4380f0fa9820770a0845fc
KAZUKI27854 <02kazuki16@gmail.com>
2021-05-26 07:31:34 +0000

    [update]order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,21 @@
 class Order < ApplicationRecord
   enum payment_method: {クレジットカード:0, 銀行振込:1}
+  enum status: {入金待ち:0, 入金確認:1, 製作中:2, 発送準備中:3, 発送済み:4}
+
+  belongs_to :customer
+  has_many :items
+
+  has_many :order_items
+
+  def order_item_status_auto_update
+    if self.status == "入金確認"
+      self.order_items.each do |order_item|
+        order_item.update_attributes(product_status: "製作待ち")
+      end
+    elsif self.status == "入金待ち"
+      self.order_items.each do |order_item|
+        order_item.update_attributes(product_status: "着手不可")
+      end
+    end
+  end
 end
