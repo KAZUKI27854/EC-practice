d96946ac7fa1ff61c504b4f82c93501ccbd0694e
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-05 06:24:40 +0000

    [add]Customers/show

---
+++ app/controllers/customer/customers_controller.rb
@@ -0,0 +1,12 @@
+class Customer::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
+  def show
+    @customer = current_customer
+  end
+  
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
+    end
+end
