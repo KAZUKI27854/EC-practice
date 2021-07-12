8b0d24f50e789f6a0f188fb9e47059e210e6f077
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-12 00:56:26 +0000

    [add]Deliveries/create

--- app/controllers/customer/deliveries_controller.rb
+++ app/controllers/customer/deliveries_controller.rb
@@ -3,4 +3,21 @@ class Customer::DeliveriesController < ApplicationController
     @delivery = Delivery.new
     @deliveries = current_customer.deliveries
   end
+  
+  def create
+    @delivery = Delivery.new(delivery_params)
+    @delivery.customer_id = current_customer.id
+    if @delivery.save
+      redirect_back(fallback_location: root_path)
+    else
+      @deliveries = current_customer.deliveries
+      render :index
+    end
+  end
+  
+  private
+
+    def delivery_params
+      params.require(:delivery).permit(:postcode, :address, :name)
+    end
 end
