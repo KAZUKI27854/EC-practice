6ed4af093c3616f84284580c510d81e40ca9ca56
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-15 23:29:08 +0000

    [add]validation to delivery.rb

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,3 +1,9 @@
 class Delivery < ApplicationRecord
   belongs_to :customer
+  
+  validates :postcode, :name, :address, presence: true
+
+  def delivery_full_address
+    '〒' + self.postcode + ' ' + self.address + ' ' + self.name
+  end
 end
