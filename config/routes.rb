e36783af7f3d1e197fc36010835444e3d60d452b
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-16 01:21:57 +0000

    [update]routing of devise_for_customers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  
+  devise_for :customers, controllers: {
+  sessions:      'customers/sessions',
+  passwords:     'customers/passwords',
+  registrations: 'customers/registrations'
+}
+
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
 
   namespace :admin do
     resources :customers
