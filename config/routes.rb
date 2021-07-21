aafcd2350637717c77725f7bee1f73e41c1c8e5d
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-21 02:10:27 +0000

    [refactor]routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   registrations: 'customers/registrations'
 }
 
-  devise_for :admins, controllers: {
+devise_for :admins, controllers: {
   sessions:      'admins/sessions',
   passwords:     'admins/passwords',
   registrations: 'admins/registrations'
