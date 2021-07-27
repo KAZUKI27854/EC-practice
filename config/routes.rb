b86b1321c19a5e5aaea3ee0f0baa0fc37f6a8c63
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-27 01:24:42 +0000

    [add]routing of admin/order_items

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ devise_for :admins, controllers: {
     resources :customers, only: [:index, :show, :edit, :update]
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    
+    resources :orders, only: [:index, :show, :update]
+    resources :order_items, only: [:update]
   end
 
   scope module: :customer do
