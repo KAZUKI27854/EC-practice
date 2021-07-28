9aa0ad9e47624dfbbd1d853fe5f06f097c4db761
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-28 01:42:31 +0000

    [update]customer_validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,6 @@ class Customer < ApplicationRecord
   has_many :deliveries
   has_many :cart_items, dependent: :destroy
   has_many :orders ,dependent: :destroy
+  
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :address, :phone_number, :postcode, presence: true
 end
