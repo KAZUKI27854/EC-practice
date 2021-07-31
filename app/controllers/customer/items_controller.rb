1acc6b7ce9f7805bbc925e183efea0f633af08ba
KAZUKI27854 <02kazuki16@gmail.com>
2021-07-31 01:38:24 +0000

    [add]customer/items_show_action

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -3,4 +3,10 @@ class Customer::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(8)
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @total_price = (@item.price * 1.1).to_i
+    @cart_item = CartItem.new
+  end
 end
