dca8fb25947d87f299d92439355c9e983aaf09e7
KAZUKI27854 <02kazuki16@gmail.com>
2021-08-04 01:23:19 +0000

    [add]create_action to admin/genres_controller

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -6,5 +6,19 @@ class Admin::GenresController < ApplicationController
     @genres = Genre.all
   end
   
+  def create
+    @genre = Genre.new(params_genre)
+    @genres = Genre.all
+    if @genre.save
+      redirect_to admin_genres_path
+    else
+      render :index
+    end
+  end
   
+  private
+
+  def params_genre
+    params.require(:genre).permit(:name)
+  end
 end
