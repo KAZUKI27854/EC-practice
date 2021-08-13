2ce3932de1578830e05eb2d0b6eae61c39904a2b
KAZUKI27854 <02kazuki16@gmail.com>
2021-08-13 02:12:19 +0000

    [add]before_action to admin/orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,3 +1,11 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
   
+  def index
+    if params[:customer_id].nil?
+      @orders = Order.page(params[:page]).reverse_order
+    else
+      @orders = Order.where(customer_id: params[:customer_id]).page(params[:page]).reverse_order
+    end
+  end
 end
