e0c59b15bc3412655ab08366d15b13c1ffc7147b
KAZUKI27854 <02kazuki16@gmail.com>
2021-08-14 03:19:02 +0000

    [add]update_action to admin/orders_controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -13,4 +13,11 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_items = @order.order_items
   end
+  
+  def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    order.order_item_status_auto_update
+    redirect_to admin_order_path(params[:id])
+  end
 end
