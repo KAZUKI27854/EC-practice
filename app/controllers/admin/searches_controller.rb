b5a4cc9b8d0ba8b77a8f29419338f6aec0dc81f1
KAZUKI27854 <02kazuki16@gmail.com>
2021-08-18 05:32:53 +0000

    [add]admin/search_action

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,3 +1,11 @@
 class Admin::SearchesController < ApplicationController
-  
+  def search
+    @range = params[:range]
+    @word = params[:word]
+    if @range == "Item"
+      @items = Item.looks(params[:word])
+    else
+      @customers = Customer.looks(params[:word])
+    end
+  end
 end
