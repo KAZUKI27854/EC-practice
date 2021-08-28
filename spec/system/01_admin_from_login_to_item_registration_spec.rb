9bb32066feb1ae18a5018a91c2195447472930c6
KAZUKI27854 <02kazuki16@gmail.com>
2021-08-28 01:42:49 +0000

    [update]spec/system/test_01

--- spec/system/01_admin_from_login_to_item_registration_spec.rb
+++ spec/system/01_admin_from_login_to_item_registration_spec.rb
@@ -15,4 +15,15 @@ describe '1.マスタ登録のテスト' do
       expect(current_path).to eq admin_orders_path
     end
   end
+  
+  context '管理者トップ画面のテスト' do
+    it 'ログイン後、ヘッダーにジャンル一覧のリンクが表示されている' do
+      expect(page).to have_link 'ジャンル一覧'
+    end
+
+    it 'ジャンル一覧のリンクを押すとジャンル一覧画面が表示される' do
+      click_link 'ジャンル一覧'
+      expect(current_path).to eq admin_genres_path
+    end
+  end
 end
