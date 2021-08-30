bec51eb76a590a7133f7816a3614dd24a8029f3e
KAZUKI27854 <02kazuki16@gmail.com>
2021-08-30 02:00:41 +0000

    [update]spec/system/test_01

--- spec/system/01_admin_from_login_to_item_registration_spec.rb
+++ spec/system/01_admin_from_login_to_item_registration_spec.rb
@@ -27,5 +27,32 @@ describe '1.マスタ登録のテスト' do
     end
   end
   
+  context '商品一覧画面のテスト' do
+    before do
+      visit admin_items_path
+    end
+
+    it '「商品作成はこちら」というリンクを押すと商品新規登録画面が表示される' do
+      click_link '商品作成はこちら'
+      expect(current_path).to eq new_admin_item_path
+    end
+  end
   
+  context '商品新規登録画面のテスト' do
+    let!(:genre) { create(:genre) }
+    before do
+      visit new_admin_item_path
+    end
+
+    it '必要事項を入力して登録ボタンを押すと登録した商品の詳細画面が表示される' do
+      attach_file 'item[image]', "#{Rails.root}/app/assets/images/strawberry-cake.jpg"
+      fill_in 'item[name]', with: 'いちごのケーキ(ホール)'
+      fill_in 'item[caption]', with: '高級いちごを贅沢に使用しています'
+      select genre.name, from: 'item[genre_id]'
+      fill_in 'item[price]', with: 3000
+      click_button '送信'
+      expect(page).to have_content '商品詳細'
+      expect(page).to have_content 'いちごのケーキ(ホール)'
+    end
+  end
 end
