76bb503f9d44c753f86a0546efad2b6b25da7f53
KAZUKI27854 <02kazuki16@gmail.com>
2021-09-05 00:07:50 +0000

    [update]spec/system/test_02

--- spec/system/02_customer_order_from_registration_spec.rb
+++ spec/system/02_customer_order_from_registration_spec.rb
@@ -11,4 +11,38 @@ describe '登録〜注文のテスト' do
       expect(current_path).to eq new_customer_registration_path
     end
   end
+  
+  describe 'トップページ以降のテスト' do
+    before do
+      visit new_customer_registration_path
+      fill_in 'customer[last_name]', with: "流美衣"
+      fill_in 'customer[first_name]', with: "恩零留頭"
+      fill_in 'customer[last_name_kana]', with: "ルビー"
+      fill_in 'customer[first_name_kana]', with: "オンレイルズ"
+      fill_in 'customer[email]', with: "ruby@onrail.com"
+      fill_in 'customer[postcode]', with: "ruby_on_rails"
+      fill_in 'customer[address]', with: "123456789"
+      fill_in 'customer[phone_number]', with: "2222222222"
+      fill_in 'customer[password]', with: "testtest"
+      fill_in 'customer[password_confirmation]', with: "testtest"
+      find(".sign-up-btn").click
+    end
+
+    context '新規登録画面のテスト' do
+      it '必要事項を入力して登録ボタンを押下するとマイページに遷移する' do
+        expect(current_path).to eq customer_path(1)
+      end
+
+      it '新規登録画面で入力した情報が表示されている' do
+        expect(page).to have_content '流美衣'
+        expect(page).to have_content '恩零留頭'
+        expect(page).to have_content 'ルビー'
+        expect(page).to have_content 'オンレイルズ'
+        expect(page).to have_content 'ruby@onrail.com'
+        expect(page).to have_content 'ruby_on_rails'
+        expect(page).to have_content '123456789'
+        expect(page).to have_content '2222222222'
+      end
+    end
+  end
 end
