3f1ace6f874dce346f8f53d76cf72fc91177bc3c
KAZUKI27854 <02kazuki16@gmail.com>
2021-09-25 12:01:34 +0000

    [update]spec/system/test_03

--- spec/system/03_admin_order_customer_order_spec.rb
+++ spec/system/03_admin_order_customer_order_spec.rb
@@ -42,6 +42,7 @@ describe '1.マスタ登録のテスト' do
   context '注文詳細画面のテスト' do
     before do
       visit admin_order_path(order.id)
+      OrderItem.create(item_id: 2, order_id: 1, quantity: 1, market_price: item_2.price, product_status: 3)
     end
 
     it '注文ステータスを入金確認に変更すると制作ステータスが制作待ちに更新される' do
